1ffeae889244b7ac0ccf22698050f5b7215749b3
Leire Ordeñana Madina <leireomadina@gmail.com>
2025-10-12 18:07:09 +0200

    Install and config vue i18n

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1>You did it!</h1>
+  <p>{{ $t('hello')}}</p>
   <p>
     Visit
     <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
+
+<script setup lang="ts">
+</script>
