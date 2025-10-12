aa16b1ec2476f8205db8284da0ba6eea9efae0d2
Leire Ordeñana Madina <leireomadina@gmail.com>
2025-10-12 19:08:50 +0200

    Add BaseIcon component and integrate Lucide icons library

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <h1>You did it!</h1>
-  <p class="text-blue-600 text-center">{{ $t('hello')}}</p>
+  <p class="text-blue-600 text-center">{{ $t('hello') }}</p>
   <p>
     Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+      rel="noopener"
+      >vuejs.org</a
+    >
+    to read the documentation
   </p>
+  <BaseIcon
+    :icon="LucideHome"
+    color="lightgreen"
+  />
 </template>
 
 <script setup lang="ts">
+  import BaseIcon from '@/components/BaseIcon.vue'
+  import { LucideHome } from 'lucide-vue-next'
 </script>
---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <component
+    :is="icon"
+    :color="color"
+    :size="size"
+  />
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    icon: {
+      // Lucide icon component
+      type: Object,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 24,
+      validator: (value: number) => value > 0,
+    },
+    color: {
+      type: String,
+      default: 'currentColor',
+    },
+  })
+</script>
