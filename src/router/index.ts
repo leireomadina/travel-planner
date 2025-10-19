b0d5b1bfeb5082841e1e8464ca1ceef0c031486a
Leire Ordeñana Madina <leireomadina@gmail.com>
2025-10-19 16:42:35 +0200

    Add routing for Home and Register views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { name: 'Home', path: '/', component: () => import('../views/LoginView.vue') },
+    { name: 'Register', path: '/register', component: () => import('../views/RegisterView.vue') },
+  ],
 })
 
 export default router
