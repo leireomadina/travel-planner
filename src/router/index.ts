f8347a4dbc2e5958c6809052dba04c0a29f0b91c
Leire Ordeñana Madina <leireomadina@gmail.com>
2025-10-26 10:52:55 +0100

    Update login route and add home view empty view

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { name: 'Home', path: '/', component: () => import('../views/LoginView.vue') },
+    { name: 'Home', path: '/', component: () => import('../views/HomeView.vue') },
+    { name: 'Login', path: '/login', component: () => import('../views/LoginView.vue') },
     { name: 'Register', path: '/register', component: () => import('../views/RegisterView.vue') },
   ],
 })
